4900243d881721f29736142b4a43cae073c3b093
mxkooo <mikoturko@wp.pl>
2023-07-14 12:59:18 +0200

    Added MP4 class and video files

---
+++ src/AbstractVideoFile.java
@@ -0,0 +1,17 @@
+public abstract class AbstractVideoFile extends AbstractFile implements VideoFile {
+    protected String title;
+    protected double length;
+
+    protected AbstractVideoFile(String name, int size, String title, double length) {
+        super(name, size);
+
+        this.title = title;
+        this.length = length;
+    }
+    @Override
+    public FileType getType() {
+        return FileType.VIDEO;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         GIFImageFile gif = new GIFImageFile("nazwa1.gif", 1);
         JPGImageFile jpg = new JPGImageFile("nazwa1.jpg", 2,80 );
         MP3MusicFile mp3 = new MP3MusicFile("plik.mp3", 4, "Marilyn Manson", "Broken Needle", 100);
+        MP4VideoFile mp4 = new MP4VideoFile("plik.mp3", 8, "Shrek", 2.5, 100);
 
         Drive drive = new HDDDrive(512, 3000);
         drive.addFile(gif);
