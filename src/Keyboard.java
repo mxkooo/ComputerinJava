ea36591d55b5de534f1b9e59b37da04567b8c24f
mxkooo <mikoturko@wp.pl>
2023-07-14 12:21:21 +0200

    Added Keyboard class

---
+++ src/Keyboard.java
@@ -0,0 +1,25 @@
+public class Keyboard implements USBDevice {
+
+    private String name;
+
+    public Keyboard(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean connect() {
+        System.out.println("Klawiatura podłączona");
+        return true;
+    }
+
+    @Override
+    public boolean disconnect() {
+        System.out.println("Klawiatura odłączona");
+        return true;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
