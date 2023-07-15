af2ef8c6fc13fecc496d41765b576a77afb125fa
mxkooo <mikoturko@wp.pl>
2023-07-15 14:57:46 +0200

    Added processor

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@ public class Main {
         JPGImageFile jpg = new JPGImageFile("nazwa1.jpg", 2,80 );
         MP3MusicFile mp3 = new MP3MusicFile("plik.mp3", 4, "Marilyn Manson", "Broken Needle", 100);
         MP4VideoFile mp4 = new MP4VideoFile("plik.mp3", 8, "Shrek", 2.5, 100);
-
-
+        Processor ryzen = new Processor(3.6, 16, "AM4");
 
         Drive drive = new HDDDrive(512, 3000);
 
